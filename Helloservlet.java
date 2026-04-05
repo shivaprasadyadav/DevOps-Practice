f16cd3a794fc1fd9db8c550d4624cb1fa90824cf
shivaprasadyadav <90260862+shivaprasadyadav@users.noreply.github.com>
2026-04-05 10:16:05 +0530

    Refactor HelloServlet package and add doPost method
    Updated package name and modified servlet methods.

--- Helloservlet.java
+++ Helloservlet.java
@@ -1,16 +1,19 @@
-package com.devops;
+package com.example;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
-import javax.servlet.*;
-import java.io.*;
 
+@WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, 
-                         HttpServletResponse response) 
-                         throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<h1>Hello from Jenkins + Tomcat!</h1>");
-        out.println("<p>Deployed successfully!</p>");
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String name = request.getParameter("username");
+
+        request.setAttribute("name", name);
+        request.getRequestDispatcher("result.jsp").forward(request, response);
     }
 }
